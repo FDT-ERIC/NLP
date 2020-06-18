9a991e759c8dc080423699c27b60271aea6a8fc3
805174018@qq.com <805174018@qq.com>
2020-06-18 12:09:05 +0800

    change random forest

--- 02_NER/Method2_RandomForest.py
+++ 02_NER/Method2_RandomForest.py
@@ -6,6 +6,13 @@ data = pd.read_csv("../Data/02_NER_Greedy/ner_dataset.csv", encoding="latin1")
 data = data.fillna(method="ffill") # 向下填充空缺数据
 
 # 提取特征，作为随机森林的特征选择
+'''
+istitle() 方法检测字符串中所有的单词拼写首字母是否为大写，且其他字母为小写
+islower() 方法检测字符串是否由小写字母组成
+isupper() 方法检测字符串是否由大写字母组成
+isdigit() 方法检测字符串是否只由数字组成
+isalpha() 方法检测字符串是否只由字母组成
+'''
 def get_feature(word):
     return np.array([word.istitle(), word.islower(), word.isupper(), len(word),
                      word.isdigit(),  word.isalpha()])
@@ -18,6 +25,7 @@ tags = data["Tag"].values.tolist()
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import classification_report
+# 参数 n_estimators 表示森林中树的数量
 pred = cross_val_predict(RandomForestClassifier(n_estimators=20), X=words, y=tags, cv=5)
 report = classification_report(y_pred=pred, y_true=tags)
 print(report)
